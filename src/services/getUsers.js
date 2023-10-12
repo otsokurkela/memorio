5b9ad94c6c54da6dc1c51de8d018a8b2c509498d
Ode42 <ode.progr@outlook.com>
2023-10-12 18:08:16 +0300

    Authorization workin

--- src/services/getUsers.js
+++ src/services/getUsers.js
@@ -1,8 +1,11 @@
 async function getUsers() {
+  console.log(localStorage.getItem('accessToken'));
   const response = await fetch('http://localhost:5000/api/users', {
     method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-    Authorization: 'Bearer ' + localStorage.getItem('accessToken')
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: 'Bearer ' + localStorage.getItem('accessToken')
+    }
   }).then(async (response) => {
     console.log(response);
   });
