68b155ff3a178bf36de17e55279ff0f149c3cd63
Ode42 <ode.progr@outlook.com>
2023-10-13 19:33:19 +0300

    Fetching user

--- src/components/Authentication/Profile.jsx
+++ src/components/Authentication/Profile.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
-import getUser from '../../services/getUser';
+import React, { useState, useEffect } from 'react';
 
 export default function Profile() {
-  getUser();
-  return <div>Profile</div>;
+  const [user, setUser] = useState();
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await fetch('http://localhost:5000/api/users', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: 'Bearer ' + localStorage.getItem('accessToken')
+        }
+      });
+      const userData = await response.json();
+      setUser(userData.user);
+    };
+    fetchUser();
+  }, []);
+  return <div>{user.user_name}</div>;
 }
--- src/services/getUser.js
+++ src/services/getUser.js
@@ -1,17 +1,16 @@
 async function getUser() {
-  console.log(localStorage.getItem('accessToken'));
+  let user = {};
   const response = await fetch('http://localhost:5000/api/users', {
     method: 'GET',
-    body: {
-      userId: localStorage.getItem('userId')
-    },
     headers: {
       'Content-Type': 'application/json',
       authorization: 'Bearer ' + localStorage.getItem('accessToken')
     }
   }).then(async (response) => {
-    console.log(response);
+    user = await response.json();
   });
+  console.log(user);
+  return user;
 }
 
 export default getUser;
